40acb58ac6fec3b166a2160e19e91af1dbb5541e
Gorhaf <gorhaf@163.com>
2021-11-08 17:46:48 +0800

    13：fail-fast 快速失败机制
    看动画，学Java集合教程13：fail-fast 快速失败机制

---
+++ part13/多线程问题/Main.java
@@ -0,0 +1,53 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * @author 【B站】人人都是程序员
+ * @author 【掘金】人人都是程序员
+ * @author 【CSDN】人人都是程序员
+ * @author 【抖音/今日头条/西瓜视频】人人都是程序员
+ * @author 【官方网站】www.gorhaf.com
+ * @author 【微信公众号】gorhaf
+ * 欢迎大家扫描下方二维码关注我们
+ * █▀▀▀▀▀▀▀██▀██████▀▀▀▀██▀▀▀▀▀▀▀█
+ * █ █▀▀▀█ █▄ ▀ ▄▄█▄█▄█▀██ █▀▀▀█ █
+ * █ █   █ █▄▄▀▄▀█▄▄ ██▀ █ █   █ █
+ * █ ▀▀▀▀▀ █ █ █▀▄▀▄ █ █ █ ▀▀▀▀▀ █
+ * █▀▀▀▀▀█▀▀▀▀▀█ █▀▄▀▀█ ▄▀█▀█▀█▀██
+ * █▄▄▄ ██▀█  █▀▀ ▀  ▄ ▄▀▄   ███▄█
+ * ██  ▀▀█▀ ▄ ▄ █▀▄▄▄▀▄▀▄ ▄██▀▀▄██
+ * █ █▄▄  ▀ █ ▄█▄▄▄▀▄▀█▄ ▄ ▄ █ █▄█
+ * █  ▄██▄▀██▄ █ ▀▄▀ ▀▀ ▄▄▄▀██▀▄██
+ * █ █▀▀█▀▀█▀ █▀▀█▄▀▀█▀▄ ▀▄    █▄█
+ * █ █▀▀██▀▀█▀  █▀▀  ██▄▀▀▀▀▀█▀ ▀█
+ * █▀▀▀▀▀▀▀█ ▄██▄▄▀▀▀▄▄▀ █▀█   ▄▄█
+ * █ █▀▀▀█ █▀ ▄█ █▀ ▄▀▄▄ ▀▀▀  ▀▀ █
+ * █ █   █ █ ▄▀▀▀█▄ ▄▀▄▀ █▀▀▀ ▀ ▄█
+ * █ ▀▀▀▀▀ █ ▀█ ▀█▄  █▄█▄▄█  ▄▀▄██
+ * ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // 创建集合
+        ArrayList<String> list = new ArrayList<>();
+        // 另一个线程修改容器
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // 添加元素
+                list.add("关注'人人都是程序员'");
+                list.add("看动画，学编程，不枯燥");
+            }
+        }).start();
+        // 获取迭代器
+        Iterator<String> iterator = list.iterator();
+        // 遍历集合
+        while (iterator.hasNext()) {
+            // 获取元素
+            System.out.println(iterator.next());
+        }
+    }
+}
